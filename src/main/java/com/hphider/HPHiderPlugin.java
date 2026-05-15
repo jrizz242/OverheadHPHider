044a2cc8ffe464a9e4b306f1aff3fd33d4245cfc
jrizz242 <jrsuper24@gmail.com>
2026-05-15 08:19:55 -0400

    renaming and setup

--- src/main/java/com/hphider/HPHiderPlugin.java
+++ src/main/java/com/hphider/HPHiderPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.hphider;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "HP Hider"
 )
-public class ExamplePlugin extends Plugin
+public class HPHiderPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private HPhiderConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    HPhiderConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(HPhiderConfig.class);
 	}
 }
--- src/test/java/com/hphider/HPHiderPluginTest.java
+++ src/test/java/com/hphider/HPHiderPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.hphider;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class HPHiderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(HPHiderPlugin.class);
 		RuneLite.main(args);
 	}
 }
